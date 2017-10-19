363be4ec16a1c1e73061b270e20c6fd86e7a7ec9
崔小存 <1597828092@qq.com>
2017-10-19 11:31:27 +0800

    Initial Commit

---
+++ iOS_unity/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS_unity
+//
+//  Created by Admin on 2017/10/19.
+//  Copyright © 2017年 桥通天下. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS_unity/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS_unity
+//
+//  Created by Admin on 2017/10/19.
+//  Copyright © 2017年 桥通天下. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+#import <ARKit/ARKit.h>
+
+@interface ViewController : UIViewController
+
+@end
